530025642b3f11949200a9805254db32d5f4dbef
Saad Elkheety <saadelkheety@gmail.com>
2020-03-06 21:32:55 +0200

    add str functions to models

--- invoice/models.py
+++ invoice/models.py
@@ -8,12 +8,19 @@ class Company(models.Model):
     tel = models.CharField(max_length=32)
     vat = models.CharField(max_length=256)
 
+    def __str__(self):
+        return f"{self.name} company"
 
 # Client : code + name + adress + initial balance
+
+
 class Client(models.Model):
     name = models.CharField(max_length=256)
     code = models.CharField(max_length=64)
     address = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 # Client Balance : Client Name + Total Sales + Total Payments + Balance (initial balance+Sales-Payments)
@@ -23,6 +30,9 @@ class Balance(models.Model):
     payments = models.FloatField()
     balance = models.FloatField()
 
+    def __str__(self):
+        return f"{self.client.name} balance is {self.balance}"
+
 
 # Product : code + product name + price
 class Product(models.Model):
@@ -30,15 +40,22 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField()
 
+    def __str__(self):
+        return f"{self.name} product"
 
  # invoice : invoice number +date + client name + client adress + product
  # + price + total + total invoice + VAT + Total VAT (total invoice * VAT)
  # + general Total invoice (Total invoice + Total VAT)
+
+
 class Invoice(models.Model):
     number = models.CharField(max_length=64)
     date = models.DateField(auto_now_add=True)
     client = models.ForeignKey('Client', on_delete=models.CASCADE)
     total = models.FloatField()
+
+    def __str__(self):
+        return f"invoice number: {self.number} of the client {self.client.name}"
 
 
 class InvoiceItem(models.Model):
@@ -48,11 +65,17 @@ class InvoiceItem(models.Model):
     quantity = models.IntegerField()
     date = models.DateField(auto_now=True)
 
-
+    def __str__(self):
+        return f"{self.product} for {self.invoice}"
  # payment : date + client name + invoice + amount paid + amount due
+
+
 class Payment(models.Model):
     client = models.ForeignKey('Client', on_delete=models.CASCADE)
     invoice = models.ForeignKey('Invoice', on_delete=models.CASCADE)
     amountDue = models.FloatField()
     amountPaid = models.FloatField()
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"payment of {self.client}"
